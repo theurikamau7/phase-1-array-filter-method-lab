aef018b30f4e1559a9fba8cc79b1918405bfc28f
IanKamau <theurikamau7@gmail.com>
2023-07-25 06:03:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+function findMatching(drivers, query) {
+    const lowercaseQuery = query.toLowerCase();
+    
+    const matchingDrivers = drivers.filter(driver => driver.toLowerCase() === lowercaseQuery);
+  
+    return matchingDrivers;
+  }
+
+  function fuzzyMatch(drivers, letters) {
+    const lowercaseLetters = letters.toLowerCase();
+    
+    const matchingDrivers = drivers.filter(driver => driver.toLowerCase().startsWith(lowercaseLetters));
+  
+    return matchingDrivers;
+  }
+
+  function matchName(driverObjects, name) {
+    const matchingDrivers = driverObjects.filter(driver => driver.name === name);
+  
+    return matchingDrivers;
+  }
